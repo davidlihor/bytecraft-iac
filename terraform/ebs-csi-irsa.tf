667fe7e3a0c440eab577d9082a638402e5a33222
David <davidlihor.dev@gmail.com>
2025-09-16 18:55:32 +0300

    fix terraform fmt

--- terraform/ebs-csi-irsa.tf
+++ terraform/ebs-csi-irsa.tf
@@ -1,9 +1,11 @@
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_name
 }
+
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
+
 module "irsa_ebs_csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "4.7.0"
@@ -13,14 +15,4 @@ module "irsa_ebs_csi" {
   provider_url                  = replace(data.aws_eks_cluster.eks.identity[0].oidc[0].issuer, "https://", "")
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  service_account_role_arn = module.irsa_ebs_csi.iam_role_arn
-
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
 }
